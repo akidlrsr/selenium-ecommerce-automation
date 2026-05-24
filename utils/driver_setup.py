377cebebe57bbe5b47529a18e9cd4bacd43070d9
Akihiro Del Rosario <akidlrsr@gmail.com>
2026-05-23 23:26:24 -0400

    Added headless mode for CI

--- utils/driver_setup.py
+++ utils/driver_setup.py
@@ -1,16 +1,21 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 def get_driver():
-    options = Options()
-    options.add_argument("--disable-notifications")
+chrome_options = Options()
 
-    prefs = {
-        "credentials_enable_service": False,
-        "profile.password_manager_enabled": False
-    }
-    options.add_experimental_option("prefs", prefs)
+```
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
 
-    driver = webdriver.Chrome(options=options)
-    driver.maximize_window()
-    return driver
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()),
+    options=chrome_options
+)
+
+driver.maximize_window()
+return driver
+```
