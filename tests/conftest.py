f402f344f8f6d50b80c265ed2feb423e0b1335dd
Akihiro Del Rosario <akidlrsr@gmail.com>
2026-05-23 23:36:45 -0400

    Added screenshot capture on failed tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,31 @@
 import pytest
 from utils.driver_setup import get_driver
+from datetime import datetime
+
 
 @pytest.fixture
 def driver():
     driver = get_driver()
+
     yield driver
+
     driver.quit()
 
+
+@pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item, call):
-    if call.when == "call" and call.excinfo is not None:
-        driver = item.funcargs.get("driver")
-        if driver:
-            driver.save_screenshot("failure.png")
+
+    outcome = yield
+    report = outcome.get_result()
+
+    if report.when == "call" and report.failed:
+
+        driver = item.funcargs["driver"]
+
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+        screenshot_name = f"screenshots/FAILED_{item.name}_{timestamp}.png"
+
+        driver.save_screenshot(screenshot_name)
+
+        print(f"\nScreenshot saved: {screenshot_name}")
