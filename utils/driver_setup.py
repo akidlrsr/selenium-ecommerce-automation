b9f3bf846340652785201ed3a9c998147db992b2
Akihiro Del Rosario <akidlrsr@gmail.com>
2026-05-23 23:28:27 -0400

    Fixed indentation in driver setup

--- utils/driver_setup.py
+++ utils/driver_setup.py
@@ -1,21 +1,16 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+
 
 def get_driver():
-chrome_options = Options()
+    chrome_options = Options()
 
-```
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome(
-    service=Service(ChromeDriverManager().install()),
-    options=chrome_options
-)
+    driver = webdriver.Chrome(options=chrome_options)
 
-driver.maximize_window()
-return driver
-```
+    driver.maximize_window()
+
+    return driver
