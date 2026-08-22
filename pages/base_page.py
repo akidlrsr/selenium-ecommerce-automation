4aeff69618cf846fe899fc010e2a05e497ff9ec2
Akihiro Del Rosario <akidlrsr@gmail.com>
2026-08-22 16:35:09 -0400

    Improve centralized Selenium waits

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,24 +1,36 @@
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class BasePage:
+
     def __init__(self, driver):
         self.driver = driver
         self.wait = WebDriverWait(driver, 10)
 
     def click(self, by, value):
-        element = self.wait.until(EC.element_to_be_clickable((by, value)))
+        element = self.wait.until(
+            EC.element_to_be_clickable((by, value))
+        )
         element.click()
 
     def type(self, by, value, text):
-        element = self.wait.until(EC.visibility_of_element_located((by, value)))
+        element = self.wait.until(
+            EC.visibility_of_element_located((by, value))
+        )
         element.clear()
         element.send_keys(text)
 
     def get_text(self, by, value):
-        element = self.wait.until(EC.visibility_of_element_located((by, value)))
+        element = self.wait.until(
+            EC.visibility_of_element_located((by, value))
+        )
         return element.text
 
     def find_elements(self, by, value):
         return self.driver.find_elements(by, value)
+
+    def wait_for_elements(self, by, value):
+        return self.wait.until(
+            EC.presence_of_all_elements_located((by, value))
+        )
--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,12 +1,19 @@
+from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 
-class CartPage:
-    def __init__(self, driver):
-        self.driver = driver
+
+class CartPage(BasePage):
 
     def is_item_in_cart(self):
-        items = self.driver.find_elements(By.CLASS_NAME, "inventory_item_name")
+        items = self.wait_for_elements(
+            By.CLASS_NAME,
+            "inventory_item_name"
+        )
+
         return len(items) > 0
 
     def remove_item(self):
-        self.driver.find_element(By.CLASS_NAME, "cart_button").click()
+        self.click(
+            By.CLASS_NAME,
+            "cart_button"
+        )
