217a53b62347a37352e00c278e67de3582caae71
Akihiro Del Rosario <akidlrsr@gmail.com>
2026-08-22 16:53:21 -0400

    Improve configurable browser and CI setup

--- utils/driver_setup.py
+++ utils/driver_setup.py
@@ -1,3 +1,5 @@
+import os
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -5,12 +7,15 @@ from selenium.webdriver.chrome.options import Options
 def get_driver():
     chrome_options = Options()
 
-    chrome_options.add_argument("--headless")
+    headless = os.getenv("HEADLESS", "true").lower() == "true"
+
+    if headless:
+        chrome_options.add_argument("--headless")
+
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(options=chrome_options)
-
-    driver.maximize_window()
 
     return driver
